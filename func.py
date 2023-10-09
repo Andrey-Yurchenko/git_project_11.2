468589db663ee1f8fd4cc0d5f6d82e4257a0c10a
Andrey <yurka210793@gmail.comgit commit -m 'initial'git config --global user.email yurka210793@gmail.com>
2023-10-09 20:49:12 +0600

    create new func

--- func.py
+++ func.py
@@ -1,3 +1,16 @@
 def foo():
+    """
+    вывод все заглавные
+    """
     user_input = input()
     return user_input.upper()
+
+def foo2():
+    """
+    вывод первые заглавные
+    :return:
+    """
+    user_input = input()
+    return user_input.title()
+
+print(foo2())
